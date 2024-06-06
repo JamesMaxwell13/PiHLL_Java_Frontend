47a5ea2b3bfb1790214ca5f6484a15579fb39b29
jamesmaxwell13 <jamesclerkmaxwell13@gmail.com>
2024-06-06 06:36:10 +0300

    add dockerfile

--- src/service/ShareService.js
+++ src/service/ShareService.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+import { process } from 'react'
 
-const REST_API_BASE_URL = 'http://localhost:8080/api/share';
+const REST_API_BASE_URL = process.env.REACT_APP_BACKEND_URL + '/api/share';
+
+// const REST_API_BASE_URL = 'http://localhost:8080/api/share';
 
 export const listShares = () => axios.get(REST_API_BASE_URL + '/all');
 
